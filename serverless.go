127d2f862db917da2b701d18b1341e9e024b04af
Ramil Aleshkin <rraleshk@mts.ru>
2022-07-04 22:29:23 +0300

    ✨ Добавил запуск через Serverless

--- serverless.go
+++ serverless.go
@@ -1,29 +1,15 @@
 package main
 
-//
-// import (
-// 	"context"
-//
-// 	"github.com/alewkinr/pingo/cmd"
-// 	"github.com/alewkinr/pingo/internal/config"
-// 	"github.com/alewkinr/pingo/internal/pingo"
-// 	"github.com/alewkinr/pingo/pkg/log"
-// 	"github.com/alewkinr/pingo/pkg/trigger"
-// )
-//
-// // Handler – обработчик для запросов Yandex.Cloud Functions
-// //nolint:unparam,deadcode
-// func Handler(ctx context.Context, r *trigger.TimerRequest) (struct{}, error) {
-// 	settings := config.MustInitConfig()
-// 	logger := log.SetUpLogging()
-//
-// 	senders := cmd.MakeSenders(settings)
-//
-// 	pinger := pingo.NewPingo(logger, senders)
-//
-// 	pingErr := pinger.Run(ctx, settings.TemplatesConfig.Templates)
-// 	if pingErr != nil {
-// 		return struct{}{}, pingErr
-// 	}
-// 	return struct{}{}, nil
-// }
+import (
+	"context"
+)
+
+// Handler – обработчик для запросов Yandex.Cloud Functions
+//nolint:unparam,deadcode
+func Handler(ctx context.Context) (struct{}, error) {
+	if runErr := run(ctx); runErr != nil {
+		return struct{}{}, runErr
+	}
+
+	return struct{}{}, nil
+}
